a58960f537c04848661cb5e21db208090fe68dbf
Anna Jordanous <annajordanous@hotmail.com>
2015-03-26 11:30:20 +0000

    initial edits from Anna including diagram of model

---
+++ figures/model-diagram/mm-model-diagram.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1427368594535" ID="ID_364760231" MODIFIED="1427368644302" TEXT="Computational model of serendipity">
+<node CREATED="1427368645022" ID="ID_1763127882" MODIFIED="1427368657163" POSITION="right" TEXT="Phases">
+<node CREATED="1427368657607" FOLDED="true" ID="ID_892080896" MODIFIED="1427368695551" TEXT="1. Discovery">
+<node CREATED="1427368679498" ID="ID_883697391" MODIFIED="1427368681092" TEXT="Within a system with a prepared mind, a previously uninteresting serendipity trigger arises due to circumstances that the system does not control, and is classified as interesting by the system"/>
+</node>
+<node CREATED="1427368661125" FOLDED="true" ID="ID_205720238" MODIFIED="1427368696375" TEXT="2. Invention">
+<node CREATED="1427368692002" ID="ID_679213768" MODIFIED="1427368693500" TEXT="The system, by subsequently processing this trigger and background information together with relevant reasoning, networking, or experimental techniques, obtains a novel result that is evaluated favourably by the system or by external sources."/>
+</node>
+</node>
+<node CREATED="1427368737682" ID="ID_1891987818" MODIFIED="1427368740894" POSITION="right" TEXT="Key condition">
+<node CREATED="1427368740895" ID="ID_486420315" MODIFIED="1427368744822" TEXT="Serendipity trigger"/>
+</node>
+<node CREATED="1427368709710" ID="ID_563045342" MODIFIED="1427368788514" POSITION="right" TEXT="Dimensions">
+<node CREATED="1427368746848" ID="ID_615978599" MODIFIED="1427368750766" TEXT="Focus shift"/>
+<node CREATED="1427368791183" ID="ID_1983960627" MODIFIED="1427368797399" TEXT="Prepared mind"/>
+<node CREATED="1427368798018" ID="ID_290478461" MODIFIED="1427368799976" TEXT="Bridge"/>
+<node CREATED="1427368801269" ID="ID_1993966731" MODIFIED="1427368803289" TEXT="Result"/>
+</node>
+<node CREATED="1427368715182" ID="ID_279544910" MODIFIED="1427368719037" POSITION="right" TEXT="Environmental Factors">
+<node CREATED="1427368847612" ID="ID_1712639855" MODIFIED="1427368850421" TEXT="Dynamic World"/>
+<node CREATED="1427368852359" ID="ID_781426624" MODIFIED="1427368863483" TEXT="Multiple contexts"/>
+<node CREATED="1427368864221" ID="ID_1737264918" MODIFIED="1427368866451" TEXT="Multiple tasks"/>
+<node CREATED="1427368867172" ID="ID_267511179" MODIFIED="1427368871099" TEXT="Multiple influences"/>
+</node>
+<node CREATED="1427368876272" ID="ID_661232882" MODIFIED="1427368877708" POSITION="right" TEXT="Chance"/>
+<node CREATED="1427368811666" ID="ID_568386286" MODIFIED="1427368838778" POSITION="right" TEXT="Curiosity"/>
+<node CREATED="1427368840137" ID="ID_710011933" MODIFIED="1427368843442" POSITION="right" TEXT="Sagacity"/>
+<node CREATED="1427368889659" ID="ID_1513225833" MODIFIED="1427368895668" POSITION="right" TEXT="Value"/>
+</node>
+</map>
